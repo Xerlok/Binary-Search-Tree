6f2f25853aab503b1e1d217aa81b3ec772c7954f
Xerlok <20791727+Xerlok@users.noreply.github.com>
2024-03-10 13:34:55 +0300

    Add basic BST generation and prettyPrint

--- src/binaryTree.js
+++ src/binaryTree.js
@@ -1,13 +1,37 @@
 /*eslint-disable*/
-import treeNode from './treeNode'
+import TreeNode from './treeNode'
 
 export default class BinaryTree {
   constructor() {
     this.root = null
     this.size = 0;
   }
-
+  
   buildTree(arr) {
-    console.log();
+    let arrStart = arr[0];
+    let arrEnd = arr.length-1;
+    this.root = this.createBST(arr, arrStart, arrEnd);
+  }
+
+  createBST(arr, start, end) {
+    if (start > end) { return null; }
+    const middle = parseInt((start + end) / 2);
+    let newNode = new TreeNode(arr[middle]);
+    newNode.leftNode = this.createBST(arr, start, middle-1);
+    newNode.rightNode = this.createBST(arr, middle+1, end);
+    return newNode;
+  }
+
+  prettyPrint(node, prefix = "", isLeft = true) {
+      if (node === null) {
+        return;
+      }
+      if (node.rightNode !== null) {
+        this.prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
+      }
+      console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+      if (node.leftNode !== null) {
+        this.prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
+      }
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import BinaryTree from './binaryTree';
 import sortArray from './sortArray';
 
 const newTree = new BinaryTree();
-
-newTree.buildTree(sortArray([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]));
+console.log(sortArray([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 234, 45, 5, 66, 567, 8888, 234, 121, 111, 789]));
+newTree.buildTree(sortArray([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 234, 45, 5, 66, 567, 8888, 234, 121, 111, 789]));
+newTree.prettyPrint(newTree.root);
+console.log(newTree.root);
