c37962a3cf235eb52ffa8407907de7c7234cbdac
Xerlok <20791727+Xerlok@users.noreply.github.com>
2024-03-10 21:14:18 +0300

    Add recursive insert fnctn

--- src/binaryTree.js
+++ src/binaryTree.js
@@ -8,18 +8,41 @@ export default class BinaryTree {
   }
   
   buildTree(arr) {
-    let arrStart = arr[0];
+    let arrStart = 0;
     let arrEnd = arr.length-1;
     this.root = this.createBST(arr, arrStart, arrEnd);
   }
 
+  // recursively create a BST from a sorted array
   createBST(arr, start, end) {
     if (start > end) { return null; }
     const middle = parseInt((start + end) / 2);
     let newNode = new TreeNode(arr[middle]);
-    newNode.leftNode = this.createBST(arr, start, middle-1);
+    newNode.leftNode = this.createBST(arr, start, middle-1); // recursion is black magic
     newNode.rightNode = this.createBST(arr, middle+1, end);
     return newNode;
+  }
+
+  insert(data, currentNode = this.root) {
+    if (this.root === null) { 
+      throw new Error('The tree is empty!');
+    }
+    if (data > currentNode.data) {
+      if (currentNode.rightNode === null) {
+        currentNode.rightNode = new TreeNode(data);
+        return;
+      }
+      currentNode = currentNode.rightNode;
+      this.insert(data, currentNode);
+    }
+    else if (data < currentNode.data) {
+      if (currentNode.leftNode === null) {
+        currentNode.leftNode = new TreeNode(data);
+        return;
+      }
+      currentNode = currentNode.leftNode;
+      this.insert(data, currentNode);
+    }
   }
 
   prettyPrint(node, prefix = "", isLeft = true) {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import BinaryTree from './binaryTree';
 import sortArray from './sortArray';
 
 const newTree = new BinaryTree();
-console.log(sortArray([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 234, 45, 5, 66, 567, 8888, 234, 121, 111, 789]));
 newTree.buildTree(sortArray([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 234, 45, 5, 66, 567, 8888, 234, 121, 111, 789]));
+newTree.insert(6);
+newTree.insert(566);
+newTree.insert(10);
+newTree.insert(565);
+newTree.insert(564);
+newTree.insert(563);
+newTree.insert(562);
 newTree.prettyPrint(newTree.root);
 console.log(newTree.root);
