c6c0e430dcd64cfbf3c273283169bd27da08d993
Xerlok <20791727+Xerlok@users.noreply.github.com>
2024-03-12 19:48:05 +0300

    Add delete function

--- src/binaryTree.js
+++ src/binaryTree.js
@@ -1,32 +1,42 @@
-/*eslint-disable*/
-import TreeNode from './treeNode'
+/* eslint-disable radix */
+/* eslint-disable */
+import TreeNode from './treeNode';
+import sortArray from './sortArray';
 
 export default class BinaryTree {
   constructor() {
-    this.root = null
+    this.root = null;
     this.size = 0;
   }
-  
-  buildTree(arr) {
-    let arrStart = 0;
-    let arrEnd = arr.length-1;
+
+  buildTree(array) {
+    let arr = sortArray(array);
+    const arrStart = 0;
+    const arrEnd = arr.length - 1;
+
     this.root = this.createBST(arr, arrStart, arrEnd);
   }
 
   // recursively create a BST from a sorted array
   createBST(arr, start, end) {
     if (start > end) { return null; }
+
     const middle = parseInt((start + end) / 2);
-    let newNode = new TreeNode(arr[middle]);
-    newNode.leftNode = this.createBST(arr, start, middle-1); // recursion is black magic
-    newNode.rightNode = this.createBST(arr, middle+1, end);
+    const newNode = new TreeNode(arr[middle]);
+
+    newNode.leftNode = this.createBST(arr, start, middle - 1); // recursion is black magic
+    newNode.rightNode = this.createBST(arr, middle + 1, end);
+
     return newNode;
   }
 
-  insert(data, currentNode = this.root) {
-    if (this.root === null) { 
+  insert(data, node = this.root) {
+    let currentNode = node;
+
+    if (this.root === null) {
       throw new Error('The tree is empty!');
     }
+
     if (data > currentNode.data) {
       if (currentNode.rightNode === null) {
         currentNode.rightNode = new TreeNode(data);
@@ -34,8 +44,7 @@ export default class BinaryTree {
       }
       currentNode = currentNode.rightNode;
       this.insert(data, currentNode);
-    }
-    else if (data < currentNode.data) {
+    } else if (data < currentNode.data) {
       if (currentNode.leftNode === null) {
         currentNode.leftNode = new TreeNode(data);
         return;
@@ -45,16 +54,76 @@ export default class BinaryTree {
     }
   }
 
-  prettyPrint(node, prefix = "", isLeft = true) {
-      if (node === null) {
-        return;
+  deleteItem(data) {
+    if (this.root === null) { throw new Error('The tree is empty!'); }
+
+    let currentNode = this.root;
+    let previousNode = null;
+    let parentBranch = null;
+
+    while (data !== currentNode.data) {
+      if (data < currentNode.data) {
+        previousNode = currentNode;
+        currentNode = currentNode.leftNode;
+        parentBranch = 'left';
+      } else if (data > currentNode.data) {
+        previousNode = currentNode;
+        currentNode = currentNode.rightNode;
+        parentBranch = 'right';
       }
-      if (node.rightNode !== null) {
-        this.prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+
+    if (currentNode === null) { throw new Error('No such data!'); }
+
+    if (currentNode.leftNode === null && currentNode.rightNode === null) {
+      if (parentBranch === 'left') {
+        previousNode.leftNode = null;
+      } else if (parentBranch === 'right') { previousNode.rightNode = null; }
+    } else if (currentNode.leftNode !== null && currentNode.rightNode === null) {
+      if (parentBranch === 'left') {
+        previousNode.leftNode = currentNode.leftNode;
+      } else if (parentBranch === 'right') { previousNode.rightNode = currentNode.leftNode; }
+    } else if (currentNode.rightNode !== null && currentNode.leftNode === null) {
+      if (parentBranch === 'left') {
+        previousNode.leftNode = currentNode.rightNode;
+      } else if (parentBranch === 'right') { previousNode.rightNode = currentNode.rightNode; }
+    } else if (currentNode.rightNode !== null && currentNode.leftNode !== null) {
+      if (currentNode.rightNode.leftNode === null) {
+        currentNode.data = currentNode.rightNode.data;
+        currentNode.rightNode = currentNode.rightNode.rightNode;
+      } else {
+        const inheritingChild = this.findSmallestChildNode(currentNode.rightNode);
+        currentNode.data = inheritingChild.node.data;
+        inheritingChild.previousNode.leftNode = inheritingChild.node.rightNode;
       }
-      console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-      if (node.leftNode !== null) {
-        this.prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
-      }
+    }
+
+  }
+
+  findSmallestChildNode(startNode) {
+    let node = startNode;
+    let previousNode = null;
+
+    while (node.leftNode !== null) {
+      previousNode = node;
+      node = node.leftNode;
+    }
+
+    return { node, previousNode }
+  }
+
+  // print tree in the console
+  prettyPrint(node, prefix = '', isLeft = true) {
+    if (node === null) { return; }
+
+    if (node.rightNode !== null) {
+      this.prettyPrint(node.rightNode, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+
+    if (node.leftNode !== null) {
+      this.prettyPrint(node.leftNode, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 /* eslint-disable */
 import './styles.css';
 import BinaryTree from './binaryTree';
-import sortArray from './sortArray';
 
+// driver code (usage example)
 const newTree = new BinaryTree();
-newTree.buildTree(sortArray([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 234, 45, 5, 66, 567, 8888, 234, 121, 111, 789]));
-newTree.insert(6);
-newTree.insert(566);
-newTree.insert(10);
-newTree.insert(565);
-newTree.insert(564);
-newTree.insert(563);
-newTree.insert(562);
+newTree.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 10, 12, 11, 15, 16, 25, 23, 45, 67, 66, 87, 99, 67, 6345, 324, 234, 45, 5, 66, 567, 8888, 234, 121, 111, 789]);
+newTree.insert(24);
+newTree.deleteItem(7);
 newTree.prettyPrint(newTree.root);
-console.log(newTree.root);
