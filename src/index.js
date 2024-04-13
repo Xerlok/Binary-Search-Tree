15f9cfed7c35e12106598b7944f9f069f329369f
Xerlok <20791727+Xerlok@users.noreply.github.com>
2024-04-14 00:37:55 +0300

    Make a stupid solution to printing nodes data

--- src/index.js
+++ src/index.js
@@ -6,18 +6,19 @@ import generateArray from './arrayGenerator';
 // driver code (usage example)
 const newTree = new BinaryTree();
 const randomArray = generateArray(20);
+let result = [];
 
 function printNodesData(node) {
-  if (node) {
-    const result = [];
     result.push(node.data);
-    console.log(result);
-  }
 }
 
 newTree.buildTree([4,4,55,4,3,2,6,7,8,87,65,54,32,12,34,45,7]);
 newTree.insert(6);
 newTree.deleteItem(5);
-newTree.levelOrder(newTree.root, (node) => printNodesData(node));
-newTree.inOrder(newTree.root, (node) => printNodesData(node));
+newTree.levelOrder(newTree.root, printNodesData);
+console.log(result);
+result = [];
+newTree.inOrder(newTree.root, printNodesData);
+console.log(result);
+result = [];
 newTree.prettyPrint(newTree.root);
